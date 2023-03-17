8835d36257392324cee1007321b06869b4da588c
Vlad031996 <111621998+Vlad031996@users.noreply.github.com>
2023-03-17 22:36:11 +0700

    Создание переменных,console

--- js/script.js
+++ js/script.js
@@ -23,3 +23,25 @@ console.log(2*4 == 8);// Сравнение пишется через ==
 const isChekced = true,
       isClose = false;
 console.log(isChekced || isClose);
+
+// Так мы создаём переменную 
+const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели', '');
+// Так мы создаём вторую объект 
+const personalMovieDB = {
+  count: numberOfFilm,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+// Создаём по 2 раза пользователю вопрос ХЗ ЗАЧЕМ
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
